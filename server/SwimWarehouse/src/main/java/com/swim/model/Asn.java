74bf27fe8f1f463883416dc84a958e13c380a743
John Hill <johknotterhill@gmail.com>
2018-06-04 09:22:28 -0700

    Dao added

---
+++ server/SwimWarehouse/src/main/java/com/swim/model/Asn.java
@@ -0,0 +1,49 @@
+package com.swim.model;
+
+public class Asn {
+
+	private int asnid;
+	private String vendorId;
+	private String expectedArrivalDate;
+	private String expectedArrivalTime;
+
+	// No Args Constructor
+	public Asn() {
+	}
+	
+	// Getters and Setter
+	public int getAsnid() {
+		return asnid;
+	}
+
+	public void setAsnid(int asnid) {
+		this.asnid = asnid;
+	}
+
+	public String getVendorId() {
+		return vendorId;
+	}
+
+	public void setVendorId(String vendorId) {
+		this.vendorId = vendorId;
+	}
+
+	public String getExpectedArrivalDate() {
+		return expectedArrivalDate;
+	}
+
+	public void setExpectedArrivalDate(String expectedArrivalDate) {
+		this.expectedArrivalDate = expectedArrivalDate;
+	}
+
+	public String getExpectedArrivalTime() {
+		return expectedArrivalTime;
+	}
+
+	public void setExpectedArrivalTime(String expectedArrivalTime) {
+		this.expectedArrivalTime = expectedArrivalTime;
+	}
+
+
+
+}
